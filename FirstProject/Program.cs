b4205e11eac581cb271f11c23368fea84d6a4fc9
Administrator <Administrator@DESKTOP-C41R0IR>
2025-02-15 13:57:21 +0800

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello, " + name + "!");
+    }
+}
